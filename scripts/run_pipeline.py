6fa8121ed5aed0b73f3f96cc88d0c0ffae604c87
ZackViros51 <davidsomagalhaes@hotmail.com>
2026-08-14 20:11:25 -0300

    feat: add staging for Diversidade florística do arboreto do JB(citacoes)

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,25 +1,39 @@
 """Executa o pipeline ETL completo"""
 
-from .clean import clean_SPP
-from .staging import stage_SPP
+from .clean import clean_SPP, clean_arboreto_citations
+from .staging import stage_SPP, stage_arboreto_citations
 
 try:
-    from .load import load_SPP
-except ImportError:  
+    from .load import load_SPP, load_arboreto_citations
+except ImportError:
     import scripts.clean.clean_SPP as clean_SPP
+    import scripts.clean.clean_arboreto_citations as clean_arboreto_citations
     import scripts.staging.stage_SPP as stage_SPP
+    import scripts.staging.stage_arboreto_citations as stage_arboreto_citations
     import scripts.load.load_SPP as load_SPP
+    import scripts.load.load_arboreto_citations as load_arboreto_citations
 
 
 def main() -> None:
-    print("== STAGING ==")
+    print("== STAGING (SPP) ==")
     stage_SPP.load_staging(stage_SPP.extract_spp())
 
-    print("\n== CLEAN ==")
+    print("\n== CLEAN (SPP) ==")
     clean_SPP.load_clean(clean_SPP.transform(clean_SPP.load_staging_df()))
 
-    print("\n== LOAD ==")
+    print("\n== LOAD (SPP) ==")
     load_SPP.run()
+
+    print("\n== STAGING (arboreto - citações) ==")
+    stage_arboreto_citations.load_staging(stage_arboreto_citations.extract_citations())
+
+    print("\n== CLEAN (arboreto - citações) ==")
+    clean_arboreto_citations.load_clean(
+        clean_arboreto_citations.transform(clean_arboreto_citations.load_staging_df())
+    )
+
+    print("\n== LOAD (arboreto - citações) ==")
+    load_arboreto_citations.run()
 
 
 if __name__ == "__main__":
